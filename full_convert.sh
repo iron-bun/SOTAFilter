65a360a92d33b529f432f507c5cd426cf6a8de1a
Chris Sinclair <chris@quarterstaff.co.uk>
2023-06-30 18:08:42 +0100

    added more full_commit entries

--- full_convert.sh
+++ full_convert.sh
@@ -19,15 +19,17 @@ then
   echo "Germany stops data not found \(sources/no_stops.txt\)"
 else
 
-./SOTAfilter.py -f json ie sources/naptan.json sources/summitslist.csv EI > data/EI.json
-./SOTAfilter.py -f json gb sources/gb_stops.csv sources/summitslist.csv G > data/G.json
-./SOTAfilter.py -f json gb sources/gb_stops.csv sources/summitslist.csv GD > data/GD.json
-./SOTAfilter.py -f json ni sources/09-05-2022busstop-list.csv sources/summitslist.csv GI > data/GI.json
-./SOTAfilter.py -f json je sources/jersey.json sources/summitslist.csv GJ > data/GJ.json
-./SOTAfilter.py -f json gb sources/gb_stops.csv sources/summitslist.csv GM > data/GM.json
-./SOTAfilter.py -f json gb sources/gb_stops.csv sources/summitslist.csv GU > data/GU.json
-./SOTAfilter.py -f json gb sources/gb_stops.csv sources/summitslist.csv GW > data/GW.json
-./SOTAfilter.py -f json no sources/no_stops.txt sources/summitslist.csv LA > data/LA.json
-./SOTAfilter.py -f json -e utf-8 de sources/de_stops.txt sources/summitslist.csv DM > data/DM.json
+time ./SOTAfilter.py -f json ie sources/naptan.json sources/summitslist.csv EI > data/EI.json
+time ./SOTAfilter.py -f json gb sources/gb_stops.csv sources/summitslist.csv G > data/G.json
+time ./SOTAfilter.py -f json im sources/iom_stops.csv sources/summitslist.csv GD > data/GD.json
+time ./SOTAfilter.py -f json ni sources/09-05-2022busstop-list.csv sources/summitslist.csv GI > data/GI.json
+time ./SOTAfilter.py -f json je sources/jersey.json sources/summitslist.csv GJ > data/GJ.json
+time ./SOTAfilter.py -f json -e utf-8 gb sources/gb_stops.csv sources/summitslist.csv GM > data/GM.json
+time ./SOTAfilter.py -f json gb sources/gb_stops.csv sources/summitslist.csv GW > data/GW.json
+time ./SOTAfilter.py -f json -e utf-8 no sources/no_stops.txt sources/summitslist.csv LA > data/LA.json
+time ./SOTAfilter.py -f json -e utf-8 de sources/de_stops.txt sources/summitslist.csv DM > data/DM.json
+time ./SOTAfilter.py -f json -e utf-8 be sources/be_stops.txt sources/summitslist.csv ON > data/ON.json
+time ./SOTAfilter.py -f json fr sources/public-transit.geojson sources/summitslist.csv F > data/F.json
+time ./SOTAfilter.py -f json fr sources/public-transit.geojson sources/summitslist.csv FL > data/FL.json
 
 fi
